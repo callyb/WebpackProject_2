b627fbe20be26251a6a10f78bfe10aa8ceb99566
CallyB <carole.nissen@gmail.com>
2022-03-04 18:03:39 +0000

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 var webpack = require('webpack');
 var path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+
+          },
+          "css-loader"
+        ]
+      }
+    ]
   }
 };
