bc3f080cbdc210952fb249734b2a65ca6a026a9d
CallyB <carole.nissen@gmail.com>
2022-03-07 15:28:24 +0000

    project complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,44 @@
 var webpack = require('webpack');
 var path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+  'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
-  ],
 
   module: {
     rules: [
       {
-        test: /\.js$/,
         use: 'babel-loader',
+        test: /\.js$/,
         exclude: /node_modules/
       },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-
-          },
-          "css-loader"
-        ]
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
